558be652fc9d4f8e5e9bbd17c18bd033334b852c
Corbin Davenport <me@corbin.io>
2018-10-12 01:37:00 -0400

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -1,5 +1,10 @@
-var style = document.createElement('link');
-style.rel = 'stylesheet';
-style.type = 'text/css';
-style.href = chrome.extension.getURL('tasks.css');
-(document.head||document.documentElement).appendChild(style);
+function injectStyles() {
+    console.log(document.querySelector("iframe"))
+    var cssLink = document.createElement("link");
+    cssLink.href = chrome.extension.getURL('tasks.css');
+    cssLink.rel = "stylesheet"; 
+    cssLink.type = "text/css"; 
+    document.querySelector("iframe").document.head.appendChild(cssLink);
+}
+
+window.addEventListener("load", injectStyles, false);
